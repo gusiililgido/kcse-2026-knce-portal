3edfe81593e3e1b6cb87a7c213e15a55e4dd6e90
gusiililgido <gusiililgido@gmail.com>
2026-05-29 21:44:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -161,3 +161,23 @@ document.getElementById("vipPopup")
 .style.display = "none";
 
 }
+
+function searchContent(){
+
+let input = document.getElementById("searchInput").value.toLowerCase();
+
+let cards = document.querySelectorAll(".card, .kjsea-card");
+
+cards.forEach(card => {
+
+let text = card.innerText.toLowerCase();
+
+if(text.includes(input)){
+card.style.display = "block";
+}else{
+card.style.display = "none";
+}
+
+});
+
+}
